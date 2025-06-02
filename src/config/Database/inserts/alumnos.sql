c62499d1f07e1e1d9c4ca9e0629d2e18805daed9
DanteLuque <danteluque706@gmail.com>
2025-06-02 11:14:35 -0500

    configuracion de la db mysql

--- src/config/Database/inserts/alumnos.sql
+++ src/config/Database/inserts/alumnos.sql
@@ -1,11 +1,11 @@
-INSERT INTO ALUMNOS (NOMBRES, APELLIDOS, created_at, updated_at) VALUES
-('Carlos', 'García', NOW(), NOW()),
-('María', 'Rodríguez', NOW(), NOW()),
-('Juan', 'Pérez', NOW(), NOW()),
-('Ana', 'López', NOW(), NOW()),
-('Luis', 'Martínez', NOW(), NOW()),
-('Sofía', 'Ramírez', NOW(), NOW()),
-('Diego', 'Fernández', NOW(), NOW()),
-('Valentina', 'Gómez', NOW(), NOW()),
-('Miguel', 'Torres', NOW(), NOW()),
-('Lucía', 'Vargas', NOW(), NOW());
+INSERT INTO ALUMNOS (NOMBRES, APELLIDOS,DNI, created_at, updated_at) VALUES
+('Carlos', 'García', '87654321', NOW(), NOW()),
+('María', 'Rodríguez', '12345678', NOW(), NOW()),
+('Juan', 'Pérez','11111111', NOW(), NOW()),
+('Ana', 'López','22222222', NOW(), NOW()),
+('Luis', 'Martínez','33333333', NOW(), NOW()),
+('Sofía', 'Ramírez', '44444444', NOW(), NOW()),
+('Diego', 'Fernández', '55555555', NOW(), NOW()),
+('Valentina', 'Gómez', '66666666', NOW(), NOW()),
+('Miguel', 'Torres','77777777', NOW(), NOW()),
+('Lucía', 'Vargas', '88888888', NOW(), NOW());
---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import db from './config/mysql/mysql.js';
+import ZombieRouter from './modules/zombie/routes/zombie.route.js';
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.zombie_path = '/api/v1/zombie';
+
+    this.connectDB();
+    this.middlewares();
+    this.routes();
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`👾 I'M ALIVE => PORT: ${this.port}`);
+    });
+  }
+
+  async connectDB() {
+    await db.testConnection();
+  }
+
+  middlewares() {
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use(this.zombie_path, ZombieRouter);
+  }
+}
+
+export default Server;
